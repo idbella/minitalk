8686b6ec4f00aff163c3b1ac768d835c5012fc52
Said Id-bella ali <sid-bell@e1r5p1.1337.ma>
2019-02-03 05:02:55 +0100

    re

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 21:31:21 by sid-bell          #+#    #+#             */
-/*   Updated: 2019/02/03 04:10:18 by sid-bell         ###   ########.fr       */
+/*   Updated: 2019/02/03 04:34:33 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 #include <signal.h>
 #include "libft/libft.h"
 
-void	ft_send2(int pid, char data)
+void	ft_send(int pid, char data)
 {
 	int i;
 	char bit;
 
-	i = 7;
-	while(i >= 0)
+	i = 0;
+	while(i < 8)
 	{
 		bit = (data >> i) & 1;
 		kill(pid, bit ? SIGUSR2 : SIGUSR1);
 		usleep(200);
-		i--;
+		i++;
 	}
 }
 
@@ -40,9 +40,9 @@ int		main(int y, char **v)
 		pid = ft_atoi(v[1]);
 		while (v[2][i])
 		{
-			ft_send2(pid, v[2][i]);
+			ft_send(pid, v[2][i]);
 			i++;
 		}
-		ft_send2(pid, 0);
+		ft_send(pid, 0);
 	}
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 21:31:21 by sid-bell          #+#    #+#             */
-/*   Updated: 2019/02/03 03:10:42 by sid-bell         ###   ########.fr       */
+/*   Updated: 2019/02/03 05:01:36 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 
 typedef struct s_data
 {
-	char 	*str;
 	char	c;
 	int		pos;
 }				t_data;
@@ -26,15 +25,12 @@ typedef struct s_data
 t_data data;
 void	ft_catch(int sig)
 {
-	int i;
-
-	i = 0;
 	if (sig == SIGUSR2)
 		data.c |= 1 << data.pos; 
-	data.pos--;
-	if (data.pos == -1)
+	data.pos++;
+	if (data.pos == 8)
 	{
-		data.pos = 7;
+		data.pos = 0;
 		if (!data.c)
 			ft_putchar('\n');
 		else
@@ -45,10 +41,8 @@ void	ft_catch(int sig)
 
 int		main()
 {
-
-	data.str = NULL;
 	data.c = 0;
-	data.pos = 7;
+	data.pos = 0;
 	ft_putnbr(getpid());
 	ft_putchar('\n');
 	signal(30, ft_catch);
